2604edf6974bebdfa3e2b5def73ea9725fb27bb8
Aleh Loup <alehloup@gmail.com>
2023-11-04 20:32:02 -0300

    implementing MSI hash table in macros...

--- ale.h
+++ ale.h
@@ -8,11 +8,14 @@
 #define assert(c)  while (!(c)) __builtin_unreachable()
 
 //useful constants
-#define _256MB 268435456
-#define _128MB 134217728
-#define _64MB   67108864
-#define _32MB   33554432
-#define _16MB   16777216
+#define _1GB   1073741824
+#define _512MB  536870912
+#define _256MB  268435456
+#define _128MB  134217728
+#define _64MB    67108864
+#define _32MB    33554432
+#define _16MB    16777216
+#define  _8MB     8388608
 #define _19_ones 1111111111111111111
 
 //better type names
@@ -29,9 +32,9 @@ typedef i32 b32; //boolean
 #define sizeof(x)       ((isize)sizeof(x))
 #define alignof(x)      ((isize)_Alignof(x))
 #define countof(a)      (sizeof(a) / sizeof(*(a)))
-#define cstrlen(s) __extension__ ({   \
-    isize cOunt_ = countof(s) - 1;    \
-    cOunt_ != 7 ? cOunt_ : strlen(s); \
+#define cstrlen(s) __extension__ ({          \
+    isize cOunt_ = countof(s) - 1;           \
+    cOunt_ != 7 ? cOunt_ : (isize)strlen(s); \
 }) // count returns pointer size for dyn cstr so... hacky fix
 
 //for shortcuts
@@ -74,9 +77,10 @@ __attribute((malloc, alloc_size(2, 4), alloc_align(3)))
 void *alloc(arena *a, isize size, isize align, isize count) {
     isize total = size * count;
     isize pad = MODPWR2(- (isize)a->beg, align); //mod -x gives n for next align
-    
-    if (count > (a->end - a->beg - pad)/size) {
-        assert(count <= (a->end - a->beg - pad)/size); //ARENA OUT OF MEMORY
+
+    if (total > (a->end - a->beg - pad)) {
+        printf("OUT OF MEMORY, Ask:%lld Avail: %lld\n", total, a->end - a->beg - pad);
+        assert(total < (a->end - a->beg - pad)); //ARENA OUT OF MEMORY
         return 0; //ARENA OUT OF MEMORY
     }
 
@@ -100,31 +104,28 @@ arena newarena(isize cap) {
     ARRAY defs and operations
 */
 
-#define ARR_HEADER_              \
-        isize len; isize cap;    \
-        isize start; b32 invalid \
-
-//dynarr have their typename as type##s, i.e: i64s
-#define def_dynarr(typ)    \
-    typedef struct typ##s{ \
-        ARR_HEADER_;       \
-        typ *data;         \
-    }typ##s                \
-//end of def_dynarr  
-
-//statarr have their typename as type##x##count, i.e: i64x10
-#define def_statarr(typ, count)   \
-    typedef struct typ##x##count{ \
-        ARR_HEADER_;              \
-        typ data[(count)];        \
-    }typ##x##count                \
-//end of def_statarr
+#define ARR_HEADER_DATA(type)     \
+        isize len; isize cap;     \
+        isize start; b32 invalid; \
+        type *data                \
 
 #define foridx(var, array) forrange(var, array.start, array.start + array.len, 1)
 #define isstaticarr(a)  (countof(a.data) > 8) //u8 dynarr count == ptr_size / 8 == 8
+#define print_arr(label, _ArR__, format) __extension__ ({              \
+    printf("\n=== %s ===\n", label);                                   \
+    printf("start:%lld len:%lld cap:%lld invalid:%ld\n",               \
+        (_ArR__).start, (_ArR__).len, (_ArR__).cap, (_ArR__).invalid); \
+    foridx(_pridx__, (_ArR__)) {                                       \
+        printf("|");                                                   \
+        printf(format, (_ArR__).data[_pridx__]);                       \
+        printf("| ");                                                  \
+    }                                                                  \
+    printf("\n----------------\n");                                    \
+    (_ArR__).invalid;                                                  \
+})
 
 void grow(void *slice /*arr struct*/, isize size, isize align, arena *a) {
-    struct{ARR_HEADER_; u8 *data;} replica = {0};
+    struct{ARR_HEADER_DATA(u8);} replica = {0};
     memcpy(&replica, slice, sizeof(replica));
 
     replica.cap *= 2; 
@@ -143,102 +144,111 @@ void grow(void *slice /*arr struct*/, isize size, isize align, arena *a) {
     memcpy(slice, &replica, sizeof(replica));
 }
 
-#define push(dynarr, arena) __extension__ ({ \
-    typeof(dynarr) *dynarr_ = &(dynarr); \
-    typeof(arena) *arena_ = &(arena); \
-    \
-    if (dynarr_->len >= dynarr_->cap) { \
-        grow(dynarr_, sizeof(*dynarr_->data), alignof(*dynarr_->data), arena_); \
-    } \
-    dynarr_->data + dynarr_->len++; \
+#define push(dynarr, arena) __extension__ ({  \
+    typeof(dynarr) dynarr_ = (dynarr);        \
+    typeof(arena) arena_ = (arena);           \
+                                              \
+    if (dynarr_->len >= dynarr_->cap) {       \
+        grow(dynarr_, sizeof(*dynarr_->data), \
+            alignof(*dynarr_->data), arena_); \
+    }                                         \
+    dynarr_->data + dynarr_->len++;           \
 })
 
 /*
     HASH and TABLE
 */
 
-#define _H_max_elems 32768 //2^15
-#define _H_MaSk 32767u //mask for power 15
-#define _H_sTep 49 //shift (64-15) for power 15
-i32 ht15_lookup(u64 hash_, i32 index_) {
-    u32 step = (u32)((hash_ >> _H_sTep) | 1); //odd makes cycle impossible
-    return (index_ + step) | _H_MaSk;
+/*
+    Cap needs to be a power of 2, an example for 2^15:
+    capmask = 32767 (2^15 - 1)
+    shiftstep = 49 (that is 64 - 15)
+*/ 
+i32 ht_lookup(u64 hash_, i32 index_, i64 shiftstep, i64 capmask) {
+    u32 step = (u32)((hash_ >> shiftstep) | 1); //odd makes cycle impossible
+    return (index_ + step) | capmask; 
 }
 
-#define HT_HEADER_DATA(_kEy, _vAl)   \
-        isize len; isize cap;        \
-        isize start; b32 invalid;    \
-        struct {_kEy key; _vAl val;} \
-        data[_H_max_elems]           \
-//end of HT_HEADER_
+#define HT_HEADER_DATA(_kEy, _vAl) ARR_HEADER_DATA(struct {_kEy key; _vAl val;})
+#define SET_HEADER_DATA(_kEy) HT_HEADER_DATA(_kEy, u8)
 
-#define SET_HEADER_DATA(_kEy)        \
-        isize len; isize cap;        \
-        isize start; b32 invalid;    \
-        struct {_kEy key;}           \
-        data[_H_max_elems]           \
-//end of HT_HEADER_
+typedef struct exp_and_cap{ i32 exp; i32 cap; }exp_and_cap;
+exp_and_cap find_good_exp_for_ht(i32 i) {
+    i32 exp = 8, n = 256;
+    while(n < i) {
+        ++exp; n*=2;
+    }
+    return (exp_and_cap){ .exp = exp+1, .cap = (n*2) - 1 };
+}
+
+#define newht(arena_, structname, maxn) __extension__ ({ \
+    structname *ht_loced_ = newx(arena_, structname);    \
+    exp_and_cap expncap = find_good_exp_for_ht(maxn);    \
+    ht_loced_->start = 64 - expncap.exp;                 \
+    ht_loced_->cap = expncap.cap;                        \
+    ht_loced_->data = newxs(arena_,                      \
+        typeof(*(ht_loced_->data)), expncap.cap + 1);    \
+    ht_loced_->len = 0;                                  \
+    ht_loced_->invalid = 0;                              \
+    ht_loced_;                                           \
+})
 
 // returns index if found, -index where it should be if not found
 #define HT_FIND_IDX(kEy, table, hashfun) __extension__ ({ \
-    assert((table).len <= _H_max_elems-1); \
-    typeof(kEy) key_ = (kEy); \
-    typeof((table).data) data_ = ((table).data); \
-    u64 h_ = hashfun(key_); \
-    i32 idx_ = 0; \
-    while(idx_ >= 0 && data_[idx_].key != key_) { \
-        idx_ = ht15_lookup(h_, idx_) \
-        if(!data_[idx_].key) { \
-            idx_ = -idx_;} \
-    } \
-    idx_; \
-}) \
-// end of HT_FIND_IDX
+    assert((table)->len <= (table)->cap);                 \
+    typeof(kEy) key_ = (kEy);                             \
+    typeof((table)->data) data_ = ((table)->data);        \
+    i64 shiftstep = (table)->start;                       \
+    i64 capmask = (table)->cap;                           \
+    u64 h_ = hashfun(key_);                               \
+    i32 idx_ = 0;                                         \
+    while(idx_ >= 0 && data_[idx_].key != key_) {         \
+        idx_ = ht_lookup(h_, idx_, shiftstep, capmask)    \
+        if(!data_[idx_].key) {                            \
+            idx_ = -idx_;                                 \
+        }                                                 \
+    }                                                     \
+    idx_;                                                 \
+})
 
 u64 hash_s8(s8 str) {
     u64 h = 0x7A5662DCDF;
     fori(str.len) { 
-        h ^= str.data[i] & 255;
-        h *= _19_ones;
+        h ^= str.data[i] & 255; h *= _19_ones;
     }
     return h ^ h>>32;
 }
 
-u64 hash_int64(u64 x) {
-    x ^= x >> 30;
-    x *= 0xbf58476d1ce4e5b9;
-    x ^= x >> 27;
-    x *= 0x94d049bb133111eb;
-    x ^= x >> 31;
-    return x;
+u64 hash_int64(i64 x_) {
+    u64 x = (u64)x_;
+    x ^= x >> 30; x *= 0xbf58476d1ce4e5b9; 
+    x ^= x >> 27; x *= 0x94d049bb133111eb; 
+    return x ^ x>>31;
 }
 
-u64 hash_int64_inverse(u64 x) {
-    x ^= x >> 31 ^ x >> 62;
-    x *= 0x319642b2d24d8ec3;
-    x ^= x >> 27 ^ x >> 54;
-    x *= 0x96de1b173f119089;
-    x ^= x >> 30 ^ x >> 60;
-    return x;
-}
-
-u32 hash_int32(u32 x)
+u64 hash_int32(i32 x_)
 {
-    x ^= x >> 16;
-    x *= 0x7feb352d;
-    x ^= x >> 15;
-    x *= 0x846ca68b;
-    x ^= x >> 16;
-    return x;
+    u32 x = (u32)x_;
+    x ^= x >> 16; x *= 0x7feb352d; 
+    x ^= x >> 15; x *= 0x846ca68b; 
+    return x ^ x>>16;
 }
 
+#define print_ht(label, _HaSH_tAble__, key_val_format) __extension__ ({  \
+    printf("\n=== %s ===\n", label);                                     \
+    printf("start:%lld len:%lld cap:%lld invalid:%ld\n",                 \
+        (_HaSH_tAble__).start, (_HaSH_tAble__).len,                      \
+        (_HaSH_tAble__).cap, (_HaSH_tAble__).invalid);                   \
+    forrange(_pridx__, 0, (_HaSH_tAble__).cap, 1) {                      \
+        if ((_HaSH_tAble__).data[_pridx__].key == 0) continue;           \
+        printf("|");                                                     \
+        printf(key_val_format,                                           \
+            (_HaSH_tAble__).data[_pridx__].key,                          \
+            (_HaSH_tAble__).data[_pridx__].val);                         \
+        printf("| ");                                                    \
+    }                                                                    \
+    printf("\n----------------\n");                                      \
+    (_HaSH_tAble__).invalid;                                             \
+})
 
-u32 hash_int32_inverse(u32 x)
-{
-    x ^= x >> 16;
-    x *= 0x43021123;
-    x ^= x >> 15 ^ x >> 30;
-    x *= 0x1d69e2a5;
-    x ^= x >> 16;
-    return x;
-}
+
--- datastructures.c
+++ datastructures.c
@@ -3,56 +3,31 @@
 #include <stdlib.h>
 #include "ale.h"
 
-def_statarr(i64, 10);
-def_statarr(i64, 164);
-def_dynarr(i64);
-
 typedef struct at{
-    ARR_HEADER_;
-    i64 *data;
+    ARR_HEADER_DATA(i64);
 }at;
+void test_push(arena scratch) {
+    at d = {};
+
+    fori(164) {
+        *push(&d, &scratch) = i;
+    }
+
+    print_arr("Vetor dinâmico", d, "%lld");
+}
 
 typedef struct table{
     HT_HEADER_DATA(i64, b32);
 }table;
-
-void test(arena scratch) {
-    i64x10 s = {.data = {1,2,3,4,5,6,7,8,9,10}, .len=10};
-    at d = {};
-
-    // printf("%d %d\n", isstaticarr(s), isstaticarr(d));
-    // printf("%lld %lld\n", countof(s.data), countof(d.data));
-
-    // foridx(idx, s) {
-    //     printf("%lld ", s.data[idx]);
-    // }
-    // printf("\n");
-
-    // foridx(idx, d) {
-    //     printf("%lld ", d.data[idx]);
-    // }
-    // printf("\n");
-
-    // fori(164) {
-    //     *push(d, scratch) = i;
-    //     printf("%lld ", d.data[i]);
-    // }
-
-    // foridx(idx, d) {
-    //     printf("%lld ", d.data[idx]);
-    // }
-    // printf("\n");
-
-    table ht = {0};
-
-    char *x = "Alessandro";
-    printf("tam: %lld \n", cstrlen("Alessandro"));
+void test_ht(arena a) {
+    table *ht = newht(&a, table, 256);
+    print_ht("Hash Table", (*ht), "%lld %ld");
 }
 
 int main() {
 
-    arena scratch = newarena(2 << 14);
-    test(scratch);
+    arena scratch = newarena(_8MB);
+    test_ht(scratch);
 
     return 0;
 }

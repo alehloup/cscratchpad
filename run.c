bec80062cbc35948ece36f194f634f217d59e394
Aleh Loup <alehloup@gmail.com>
2023-11-08 15:59:01 -0300

    organizing folder

--- run.c
+++ run.c
@@ -1,7 +1,15 @@
 #include "ale.h"
 
 int main(MAINARGS) {
-    shellrun("gcc @Flags %s -o %s.exe && %s.exe", _$1, _$1, _$1);
+    shellrun(
+        "gcc @Flags %s -o \"./builds/%s.exe\"" 
+        " && " 
+        "\"./builds/%s.exe\""
+            " | "
+            "tee \"./outputs/%sout.txt\""
+            
+        , _$1, _$1, _$1, _$1
+    );
 
     return 0;
 }

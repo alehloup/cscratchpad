b8f51ae474528505cf55c95318626a349fa33e05
Aleh Loup <alehloup@gmail.com>
2023-11-15 19:02:09 -0300

    fazendo problema set

--- scratch.c
+++ scratch.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include "ale.h"
 
+static unsigned char table[536870913] = {};
+
 int32_t main(int32_t argc, cstring *argv) {
 
-    unsigned char *S = calloc(268435457, sizeof(char));
-    S[268435456] = 5;
-    S[268435455] = 2;
+    unsigned char *S = table; //calloc(536870913, sizeof(char));
+    S[536870910] = 5;
+    S[536870909] = 2;
 
-    printf("d: %d", S[268435456]);
+    printf("d: %d", S[536870910]);
 
     int i = 0;
     for (; !S[i]; ++i) {
         /* empty */
     }
-    printf(" d: %d", S[268435455]);
+    printf(" d: %d", S[536870909]);
 
-    free(S);
+    //free(S);
 
     return 0;
 }

18cd9d31eab313f0f77bffc169e0e385f94d4fd5
Aleh Loup <alehloup@gmail.com>
2023-11-17 14:35:58 -0300

    add C/C++ shim for static size in array params

--- ale.h
+++ ale.h
@@ -51,17 +51,24 @@ typedef union union32_t{
 }union32_t;
 
 /*
-    Auto + _Thread_local + Better Assert + Scope/Defer
+    C/C++ compatibility shims
 */
-// auto
 #ifdef __cplusplus
     #define auto auto
     #define _Thread_local thread_local
+    #define _at_least_ /* static array size not supported in C++ */
+    #define _vla_param(_vla_size_) /* vla parameter not supported in C++ */
 #endif 
 #ifndef __cplusplus
     #define auto __auto_type
     #define thread_local _Thread_local
+    #define _at_least_ static /* usage in array/pointer parameters: buffer[static 512] */
+    #define _vla_param(_vla_size_) static _vla_size_ /* usage in vla parameters: buffer[buffer_len] */
 #endif
+
+/*
+    Better Assert + Scope/Defer
+*/
 #define ale_assert(_cond_, ...)                          \
     if (!(_cond_)) {                                     \
         ale_printf("\n!! [%s:%d] ", __FILE__, __LINE__); \
@@ -69,14 +76,14 @@ typedef union union32_t{
         ale_printf(" !!\n");                             \
         __builtin_unreachable();                         \
     }
-static _Thread_local char MACRO_scoped__;
+static thread_local char MACRO_scoped__;
 //TRICK scope that "opens" at start, and "closes" at end 
 #define scoped(start, end) MACRO_scoped__ = 1; for(start; MACRO_scoped__; (--MACRO_scoped__), end)
 
 /*
     SHELL
 */
-static int32_t shellrun(char buffer [512], cstring format, ...) {
+static int32_t shellrun(char buffer [_at_least_ 512], cstring format, ...) {
     ale_memset(buffer, '\0', 512);
 
     va_list args; va_start(args, format);
@@ -103,7 +110,7 @@ static int32_t fit_pwr2_exp(int32_t size) {
 }
 
 // RANDOM
-static int32_t RND(uint64_t seed[1]) {
+static int32_t RND(uint64_t seed[_at_least_ 1]) {
     *seed = *seed * 0x9b60933458e17d7dLL + 0xd737232eeccdf7edLL;
     int32_t shift = 29 - (uint32_t)(*seed >> 61);
     
@@ -114,7 +121,7 @@ static int32_t RND(uint64_t seed[1]) {
     ARENA
 */
 typedef struct arena{ uint8_t *beg; uint8_t *end; }arena;
-static arena newarena(int64_t buffer_len, uint8_t buffer[]) {
+static arena newarena(int64_t buffer_len, uint8_t buffer[_vla_param(buffer_len)]) {
     arena a = {};
     a.beg = (uint8_t *)buffer;
     a.end = a.beg ? a.beg + buffer_len : 0;
@@ -122,7 +129,7 @@ static arena newarena(int64_t buffer_len, uint8_t buffer[]) {
 }
 
 __attribute((malloc, alloc_size(2, 4), alloc_align(3)))
-static pointer alloc(arena a[1], int64_t size, int64_t align, int64_t count) {
+static pointer alloc(arena a[_at_least_ 1], int64_t size, int64_t align, int64_t count) {
     int64_t total = size * count;
     int64_t pad = MODPWR2(- (int64_t)a->beg, align); //mod -x gives n for next align
 
@@ -140,7 +147,7 @@ static pointer alloc(arena a[1], int64_t size, int64_t align, int64_t count) {
 */
 typedef int64_t vector64_element;
 typedef struct vector64{int32_t cap; int32_t len; int64_t *data;}vector64;
-static void grow64(vector64 dynarray[1],  arena a[1]) { 
+static void grow64(vector64 dynarray[_at_least_ 1],  arena a[_at_least_ 1]) { 
     static const int32_t DYNA_FIRST_SIZE = 64;
 
     if (!dynarray->data) {
@@ -162,7 +169,7 @@ static void grow64(vector64 dynarray[1],  arena a[1]) {
 
 typedef int32_t vector32_element;
 typedef struct vector32{int32_t cap; int32_t len; int32_t *data;}vector32;
-static void grow32(vector32 dynarray[1],  arena a[1]) { 
+static void grow32(vector32 dynarray[_at_least_ 1],  arena a[_at_least_ 1]) { 
     static const int32_t DYNA_FIRST_SIZE = 64;
 
     if (!dynarray->data) {
@@ -185,34 +192,34 @@ static void grow32(vector32 dynarray[1],  arena a[1]) {
 /*
     PUSH TO GROWABLE ARRAY
 */
-static void push_i64(vector64 dynarray[1], arena a[1], int64_t int64) {
+static void push_i64(vector64 dynarray[_at_least_ 1], arena a[_at_least_ 1], int64_t int64) {
     if (dynarray->len >= dynarray->cap) {
         grow64(dynarray, a);
     }
 
     dynarray->data[dynarray->len++] = int64;
 }
-static void push_f64(vector64 dynarr[1], arena a[1], float64_t float64) {
+static void push_f64(vector64 dynarr[_at_least_ 1], arena a[_at_least_ 1], float64_t float64) {
     int64_t replica;
     ale_memcpy(&replica, &float64, sizeof(replica)); //type prunning
 
     push_i64(dynarr, a, replica);
 }
-static void push_cstr(vector64 dynarr[1], arena a[1], cstring cstr) {
+static void push_cstr(vector64 dynarr[_at_least_ 1], arena a[_at_least_ 1], cstring cstr) {
     push_i64(dynarr, a, (int64_t) cstr);
 }
-static void push_ptr(vector64 dynarr[1], arena a[1], pointer ptr) {
+static void push_ptr(vector64 dynarr[_at_least_ 1], arena a[_at_least_ 1], pointer ptr) {
     push_i64(dynarr, a, (int64_t) ptr);
 }
 
-static void push_i32(vector32 dynarray[1], arena a[1], int32_t int32) {
+static void push_i32(vector32 dynarray[_at_least_ 1], arena a[_at_least_ 1], int32_t int32) {
     if (dynarray->len >= dynarray->cap) {
         grow32(dynarray, a);
     }
 
     dynarray->data[dynarray->len++] = int32;
 }
-static void push_f32(vector32 dynarr[1], arena a[1], float32_t float32) {
+static void push_f32(vector32 dynarr[_at_least_ 1], arena a[_at_least_ 1], float32_t float32) {
     int32_t replica;
     ale_memcpy(&replica, &float32, sizeof(replica)); //type prunning
 
@@ -222,31 +229,31 @@ static void push_f32(vector32 dynarr[1], arena a[1], float32_t float32) {
 /*
     POP GROWABLE ARRAY
 */
-static int64_t pop_i64(vector64 dynarray[1]) {
+static int64_t pop_i64(vector64 dynarray[_at_least_ 1]) {
     ale_assert(dynarray->len > 0, "POP ON 64bit EMPTY ARRAY");
 
     return dynarray->data[--dynarray->len];
 }
-static float64_t pop_f64(vector64 dynarr[1]) {
+static float64_t pop_f64(vector64 dynarr[_at_least_ 1]) {
     int64_t replica = pop_i64(dynarr);
     float64_t val = 0;
     ale_memcpy(&val, &replica, sizeof(replica)); //type prunning
 
     return val; 
 }
-static cstring pop_cstr(vector64 dynarr[1]) {
+static cstring pop_cstr(vector64 dynarr[_at_least_ 1]) {
      return (cstring) pop_i64(dynarr);
 }
-static pointer pop_ptr(vector64 dynarr[1]) {
+static pointer pop_ptr(vector64 dynarr[_at_least_ 1]) {
      return (pointer) pop_i64(dynarr);
 }
 
-static int32_t pop_i32(vector32 dynarray[1]) {
+static int32_t pop_i32(vector32 dynarray[_at_least_ 1]) {
     ale_assert(dynarray->len > 0, "POP ON 32bit EMPTY ARRAY");
 
     return dynarray->data[--dynarray->len];
 }
-static float32_t pop_f32(vector32 dynarr[1]) {
+static float32_t pop_f32(vector32 dynarr[_at_least_ 1]) {
     int32_t replica = pop_i32(dynarr);
     float32_t val = 0;
     ale_memcpy(&val, &replica, sizeof(replica)); //type prunning
@@ -257,22 +264,22 @@ static float32_t pop_f32(vector32 dynarr[1]) {
 /*
     GROWABLE ARRAY DATA AS
 */
-static int64_t * vector_data_as_i64(vector64 *dynamic_array) {
+static int64_t * vector_data_as_i64(vector64 dynamic_array[_at_least_ 1]) {
     return (int64_t *) dynamic_array->data;
 }
-static float64_t * vector_data_as_f64(vector64 *dynamic_array) {
+static float64_t * vector_data_as_f64(vector64 dynamic_array[_at_least_ 1]) {
     return (float64_t *) dynamic_array->data;
 }
-static cstring * vector_data_as_cstring(vector64 *dynamic_array) {
+static cstring * vector_data_as_cstring(vector64 dynamic_array[_at_least_ 1]) {
     return (cstring *) dynamic_array->data;
 }
-static pointer * vector_data_as_ptr(vector64 *dynamic_array) {
+static pointer * vector_data_as_ptr(vector64 dynamic_array[_at_least_ 1]) {
     return (pointer *) dynamic_array->data;
 }
-static int32_t * vector_data_as_i32(vector32 *dynamic_array) {
+static int32_t * vector_data_as_i32(vector32 dynamic_array[_at_least_ 1]) {
     return (int32_t *) dynamic_array->data;
 }
-static float32_t * vector_data_as_f32(vector32 *dynamic_array) {
+static float32_t * vector_data_as_f32(vector32 dynamic_array[_at_least_ 1]) {
     return (float32_t *) dynamic_array->data;
 }
 
@@ -318,7 +325,7 @@ typedef struct msiht64{
     int32_t shift;int32_t mask; int32_t len;
     entry_i64_i64 *data;
 }msiht64;
-static msiht64 * newmsi64(arena a[1], int32_t expected_maxn) {
+static msiht64 * newmsi64(arena a[_at_least_ 1], int32_t expected_maxn) {
     const int32_t msi_expo = fit_pwr2_exp(expected_maxn + 64);
     ale_assert(msi_expo <= 24, "%d IS TOO BIG FOR MSI, MAX IS 2^24 - 1", expected_maxn);
 
@@ -339,7 +346,7 @@ typedef struct msiht32{
     int32_t shift;int32_t mask; int32_t len;
     entry_i32_i32 *data;
 }msiht32;
-static msiht32 * newmsi32(arena a[1], int32_t expected_maxn) {
+static msiht32 * newmsi32(arena a[_at_least_ 1], int32_t expected_maxn) {
     const int32_t msi_expo = fit_pwr2_exp(expected_maxn + 64);
     ale_assert(msi_expo <= 24, "%d IS TOO BIG FOR MSI, MAX IS 2^24 - 1", expected_maxn);
 
@@ -360,7 +367,7 @@ static msiht32 * newmsi32(arena a[1], int32_t expected_maxn) {
 */
 // Finds the index of |keyi64| in the msi |table|, creates key if |create_if_not_found| is true
 static int32_t msiki(
-    msiht64 *ht /* msi_ht */, 
+    msiht64 ht[_at_least_ 1] /* msi_ht */, 
     int64_t keyi64, int32_t create_if_not_found
 ) {
     ale_assert(ht, "MSI KI TABLE IS NULL");
@@ -390,68 +397,68 @@ static int32_t msiki(
 }
 
 // Returns the index of |ikey| in the msi |table|
-static int32_t msiki_get_idx(msiht64 *table, int64_t ikey) {
+static int32_t msiki_get_idx(msiht64 table[_at_least_ 1], int64_t ikey) {
     return msiki(table, ikey, 0);
 }
 
-static int64_t msiki_get_i64(msiht64 *table, int64_t ikey) {
+static int64_t msiki_get_i64(msiht64 table[_at_least_ 1], int64_t ikey) {
     return table->data[msiki(table, ikey, 0)].val;
 }
-static float64_t msiki_get_f64(msiht64 *table, int64_t ikey) {
+static float64_t msiki_get_f64(msiht64 table[_at_least_ 1], int64_t ikey) {
     int64_t replica = table->data[msiki(table, ikey, 0)].val;
     float64_t val = 0;
     ale_memcpy(&val, &replica, sizeof(replica)); //type prunning
 
     return val;
 }
-static cstring msiki_get_cstr(msiht64 *table, int64_t ikey) {
+static cstring msiki_get_cstr(msiht64 table[_at_least_ 1], int64_t ikey) {
     return (itocstr) table->data[msiki(table, ikey, 0)].val;
 }
-static pointer msiki_get_ptr(msiht64 *table, int64_t ikey) {
+static pointer msiki_get_ptr(msiht64 table[_at_least_ 1], int64_t ikey) {
     return (pointer) table->data[msiki(table, ikey, 0)].val;
 }
 
 // Creates key if not found, then returns the index of |ikey| in the msi |table|
-static int32_t msiki_set_idx(msiht64 *table, int64_t ikey) {
+static int32_t msiki_set_idx(msiht64 table[_at_least_ 1], int64_t ikey) {
     return msiki(table, ikey, 1);
 }
 
-static int64_t msiki_set_i64(msiht64 *table, int64_t ikey, int64_t ival) {
+static int64_t msiki_set_i64(msiht64 table[_at_least_ 1], int64_t ikey, int64_t ival) {
     table->data[msiki(table, ikey, 1)].val = ival;
     return ival;
 }
-static float64_t msiki_set_f64(msiht64 *table, int64_t ikey, float64_t dval) {
+static float64_t msiki_set_f64(msiht64 table[_at_least_ 1], int64_t ikey, float64_t dval) {
     int64_t replica;
     ale_memcpy(&replica, &dval, sizeof(replica)); //type prunning
 
     table->data[msiki(table, ikey, 1)].val = replica;
     return dval;
 }
-static cstring msiki_set_cstr(msiht64 *table, int64_t ikey, cstring sval) {
+static cstring msiki_set_cstr(msiht64 table[_at_least_ 1], int64_t ikey, cstring sval) {
     table->data[msiki(table, ikey, 1)].val = (cstrtoi) sval;
     return sval;
 }
-static pointer msiki_set_ptr(msiht64 *table, int64_t ikey, pointer pval) {
+static pointer msiki_set_ptr(msiht64 table[_at_least_ 1], int64_t ikey, pointer pval) {
     table->data[msiki(table, ikey, 1)].val = (int64_t) pval;
     return pval;
 }
 
-static entry_i64_i64 * msiki_data_as_int64(msiht64 *table) {
+static entry_i64_i64 * msiki_data_as_int64(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i64_i64 *) data;
 }
 typedef struct entry_i64_f64{int64_t key; float64_t val;}entry_i64_f64;
-static entry_i64_f64 * msiki_data_as_f64(msiht64 *table) {
+static entry_i64_f64 * msiki_data_as_f64(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i64_f64 *) data;
 }
 typedef struct entry_i64_cstr{int64_t key; cstring val;}entry_i64_cstr;
-static entry_i64_cstr * msiki_data_as_cstr(msiht64 *table) {
+static entry_i64_cstr * msiki_data_as_cstr(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i64_cstr *) data;
 }
 typedef struct entry_i64_ptr{int64_t key; pointer val;}entry_i64_ptr;
-static entry_i64_ptr * msiki_data_as_ptr(msiht64 *table) {
+static entry_i64_ptr * msiki_data_as_ptr(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i64_ptr *) data;
 }
@@ -461,7 +468,7 @@ static entry_i64_ptr * msiki_data_as_ptr(msiht64 *table) {
 */
 // Finds the index of |keycstr| in the msi |table|, creates key if |create_if_not_found| is true
 static int32_t msiks(
-    msiht64 *ht /* msi_ht */, 
+    msiht64 ht[_at_least_ 1] /* msi_ht */, 
     cstring keycstr, int32_t create_if_not_found
 ) {
     ale_assert(ht, "MSI KS TABLE IS NULL");
@@ -490,69 +497,69 @@ static int32_t msiks(
     return index; // index of entry found OR entry empty
 }
 // Returns the index of |ikey| in the msi |table|
-static int32_t msiks_get_idx(msiht64 *table, cstring skey) {
+static int32_t msiks_get_idx(msiht64 table[_at_least_ 1], cstring skey) {
     return msiks(table, skey, 0);
 }
 
-static int64_t msiks_get_i64(msiht64 *table, cstring skey) {
+static int64_t msiks_get_i64(msiht64 table[_at_least_ 1], cstring skey) {
     return (int64_t) table->data[msiks(table, skey, 0)].val;
 }
-static float64_t msiks_get_f64(msiht64 *table, cstring skey) {
+static float64_t msiks_get_f64(msiht64 table[_at_least_ 1], cstring skey) {
     int64_t replica = table->data[msiks(table, skey, 0)].val;
     float64_t val = 0;
     ale_memcpy(&val, &replica, sizeof(replica)); //type prunning
 
     return val;
 }
-static cstring msiks_get_cstr(msiht64 *table, cstring skey) {
+static cstring msiks_get_cstr(msiht64 table[_at_least_ 1], cstring skey) {
     return (itocstr) table->data[msiks(table, skey, 0)].val;
 }
-static pointer msiks_get_ptr(msiht64 *table, cstring skey) {
+static pointer msiks_get_ptr(msiht64 table[_at_least_ 1], cstring skey) {
     return (pointer) table->data[msiks(table, skey, 0)].val;
 }
 
 // Creates key if not found, then returns the index of |skey| in the msi |table|
-static int32_t msiks_set_idx(msiht64 *table, cstring skey) {
+static int32_t msiks_set_idx(msiht64 table[_at_least_ 1], cstring skey) {
     return msiks(table, skey, 1);
 }
 
-static int64_t msiks_set_i64(msiht64 *table, cstring skey, int64_t ival) {
+static int64_t msiks_set_i64(msiht64 table[_at_least_ 1], cstring skey, int64_t ival) {
     table->data[msiks(table, skey, 1)].val = ival;
     return ival;
 }
-static float64_t msiks_set_f64(msiht64 *table, cstring skey, float64_t dval) {
+static float64_t msiks_set_f64(msiht64 table[_at_least_ 1], cstring skey, float64_t dval) {
     int64_t replica;
     ale_memcpy(&replica, &dval, sizeof(replica)); //type prunning
 
     table->data[msiks(table, skey, 1)].val = replica;
     return dval;
 }
-static cstring msiks_set_cstr(msiht64 *table, cstring skey, cstring sval) {
+static cstring msiks_set_cstr(msiht64 table[_at_least_ 1], cstring skey, cstring sval) {
     table->data[msiks(table, skey, 1)].val = (cstrtoi) sval;
     return sval;
 }
-static pointer msiks_set_ptr(msiht64 *table, cstring skey, pointer pval) {
+static pointer msiks_set_ptr(msiht64 table[_at_least_ 1], cstring skey, pointer pval) {
     table->data[msiks(table, skey, 1)].val = (int64_t) pval;
     return pval;
 }
 
 typedef struct entry_cstr_i64{cstring key; int64_t val;}entry_cstr_i64;
-static entry_cstr_i64 * msiks_data_as_int64(msiht64 *table) {
+static entry_cstr_i64 * msiks_data_as_int64(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_cstr_i64 *) data;
 }
 typedef struct entry_cstr_f64{cstring key; float64_t val;}entry_cstr_f64;
-static entry_cstr_f64 * msiks_data_as_f64(msiht64 *table) {
+static entry_cstr_f64 * msiks_data_as_f64(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_cstr_f64 *) data;
 }
 typedef struct entry_cstr_cstr{cstring key; cstring val;}entry_cstr_cstr;
-static entry_cstr_cstr * msiks_data_as_cstr(msiht64 *table) {
+static entry_cstr_cstr * msiks_data_as_cstr(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_cstr_cstr *) data;
 }
 typedef struct entry_cstr_ptr{cstring key; pointer val;}entry_cstr_ptr;
-static entry_cstr_ptr * msiks_data_as_ptr(msiht64 *table) {
+static entry_cstr_ptr * msiks_data_as_ptr(msiht64 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_cstr_ptr *) data;
 }
@@ -561,7 +568,7 @@ static entry_cstr_ptr * msiks_data_as_ptr(msiht64 *table) {
     MSI Hash Table with Integer32 as Key
 */
 static int32_t msiki32(
-    msiht32 *ht /* msi_ht */, 
+    msiht32 ht[_at_least_ 1] /* msi_ht */, 
     int32_t keyi32, int32_t create_if_not_found
 ) {
     ale_assert(ht, "MSI KI32 TABLE IS NULL");
@@ -591,14 +598,14 @@ static int32_t msiki32(
 }
 
 // Returns the index of |ikey| in the msi |table|
-static int32_t msiki32_get_idx(msiht32 *table, int32_t ikey) {
+static int32_t msiki32_get_idx(msiht32 table[_at_least_ 1], int32_t ikey) {
     return msiki32(table, ikey, 0);
 }
 
-static int32_t msiki_get_i32(msiht32 *table, int32_t ikey) {
+static int32_t msiki_get_i32(msiht32 table[_at_least_ 1], int32_t ikey) {
     return table->data[msiki32(table, ikey, 0)].val;
 }
-static float32_t msiki_get_f32(msiht32 *table, int32_t ikey) {
+static float32_t msiki_get_f32(msiht32 table[_at_least_ 1], int32_t ikey) {
     int32_t replica = table->data[msiki32(table, ikey, 0)].val;
     float32_t val = 0;
     ale_memcpy(&val, &replica, sizeof(replica)); //type prunning
@@ -607,15 +614,15 @@ static float32_t msiki_get_f32(msiht32 *table, int32_t ikey) {
 }
 
 // Creates key if not found, then returns the index of |ikey| in the msi |table|
-static int32_t msiki32_set_idx(msiht32 *table, int32_t ikey) {
+static int32_t msiki32_set_idx(msiht32 table[_at_least_ 1], int32_t ikey) {
     return msiki32(table, ikey, 1);
 }
 
-static int32_t msiki_set_i32(msiht32 *table, int32_t ikey, int32_t ival) {
+static int32_t msiki_set_i32(msiht32 table[_at_least_ 1], int32_t ikey, int32_t ival) {
     table->data[msiki32(table, ikey, 1)].val = ival;
     return ival;
 }
-static float32_t msiki_set_f32(msiht32 *table, int32_t ikey, float32_t dval) {
+static float32_t msiki_set_f32(msiht32 table[_at_least_ 1], int32_t ikey, float32_t dval) {
     int32_t replica;
     ale_memcpy(&replica, &dval, sizeof(replica)); //type prunning
 
@@ -623,12 +630,12 @@ static float32_t msiki_set_f32(msiht32 *table, int32_t ikey, float32_t dval) {
     return dval;
 }
 
-static entry_i32_i32 * msiki_data_as_int32(msiht32 *table) {
+static entry_i32_i32 * msiki_data_as_int32(msiht32 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i32_i32 *) data;
 }
 typedef struct entry_i32_f32{int32_t key; float32_t val;}entry_i32_f32;
-static entry_i32_f32 * msiki_data_as_f32(msiht32 *table) {
+static entry_i32_f32 * msiki_data_as_f32(msiht32 table[_at_least_ 1]) {
     auto data = table->data;
     return (entry_i32_f32 *) data;
 }

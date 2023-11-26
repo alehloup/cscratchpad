ad442504a5b19ce6b1c54cf94e7b58cf7cbf0724
Aleh Loup <alehloup@gmail.com>
2023-11-26 02:13:38 -0300

    file operations

--- ale_io.h
+++ ale_io.h
@@ -34,21 +34,17 @@ $fun mstr_t file_to_buffer(arena_t arena[1], cstr_t filename) {
     {FILE *f = 0; int32_t err = 
     fopen_s(&f, filename, "rb");
     
-        if (err) {
-            return contents;
-        }
+        assert(!err && "Could not open file for reading");
     
         fseek(f, 0, SEEK_END);
         int32_t fsize = ftell(f);
         fseek(f, 0, SEEK_SET);
 
         contents = (mstr_t) alloc(arena, 1LL, fsize+1);
-        if (!contents) {
-            fclose(f);
-            return contents;
-        }
-        int32_t bytesread = (int32_t) fread_noex(contents, 1LL, fsize, f);
-        assert(bytesread == fsize && "could not read fsize"); 
+        assert(contents && "Could not allocate buffer");
+
+        int64_t bytesread = fread_noex(contents, 1LL, fsize, f);
+        assert(bytesread == fsize && "could not read fsize#bytes"); 
         
         contents[fsize] = 0;
 
@@ -56,4 +52,44 @@ $fun mstr_t file_to_buffer(arena_t arena[1], cstr_t filename) {
     }
     
     return contents;
+}
+
+$nonnull b32_t buffer_to_file(cstr_t buffer, cstr_t filename) {
+    {FILE *f = 0; int32_t err = 
+    fopen_s(&f, filename, "wb");
+
+        assert(!err && "Could not open file for writting");
+
+        int64_t fsize = strlen(buffer);
+        int64_t bytes_written = fwrite(buffer, 1, fsize, f);
+        assert(bytes_written == fsize && "could not write fsize#bytes");
+
+    fclose(f);
+    }
+    return True;
+}
+
+$nonnull b32_t lines_to_file(vector64_t lines, cstr_t filename) {
+    {FILE *f = 0; int32_t err = 
+    fopen_s(&f, filename, "wb");
+
+        assert(!err && "Could not open file for writting");
+
+        cstr_t *data = vec_data_as_cstr(&lines);
+        int64_t bytes_written = 0;
+        cstr_t line = "";
+        int64_t fsize = 0;
+
+        for (int i = 0; i < lines.len; ++i) {
+            line = data[i]; 
+
+            fsize = strlen(line);
+            bytes_written = fwrite(line, 1, fsize, f);
+            bytes_written += fwrite("\n", 1, 1, f);
+            assert(bytes_written == fsize + 1 && "could not write fsize#bytes");
+        }
+
+    fclose(f);
+    }
+    return True;
 }
--- files.c
+++ files.c
@@ -9,13 +9,16 @@ int main(void) {
     printf("%s", contents);
 
     vector64_t lines = slice_into_nonempty_lines(&perm, contents);
+    cstr_t *data = vec_data_as_cstr(&lines);
 
-    qsort(lines.data, (uint64_t) lines.len, 
+    qsort(data, (uint64_t) lines.len, 
         sizeof(int64_t), void_compare_strings);
 
-    printf("\npos sort\n");
-
     for (int i = 0; i < lines.len; ++i) {
-        printf("%d: %s\n", i, (cstr_t) lines.data[i]);
+        printf("%d: %s\n", i, data[i]);
     }
+
+    buffer_to_file(data[0], "first.txt");
+
+    lines_to_file(lines, "sorted.txt");
 }

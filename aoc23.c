b34c5da1cc3add73a22bf095dd4cc07aec534c4f
Aleh Loup <alehloup@gmail.com>
2023-12-02 22:05:41 -0300

    aoc 2

--- aoc23.c
+++ aoc23.c
@@ -4,58 +4,39 @@ _proc_rbuffer(1, 2)
 aoc3(i32 lineslen, mstr lines[]) {
     static u8 buff[1*MBs_] = {0};
     Arena arena = newarena(1*MBs_, buff);
-    i32 possibles = 0;
-    i8 RED = 0, GREEN = 1, BLUE = 2, totals[3] = {0};
-    totals[RED] = 12; totals[GREEN] = 13; totals[BLUE] = 14;
     
-    printf("total_reds: %d, total_greens: %d, total_blues: %d\n", totals[RED], totals[GREEN], totals[BLUE]);
-
+    i8 RED = (i8)'r'-'b', GREEN = (i8)'g'-'b', BLUE = (i8)'b'-'b';
+    i64 powersum = 0;
     for (int iline = 0; iline < lineslen; ++iline) {
-        i32 game_id = iline + 1;
-        Vec64 game = mutslice_by_splitter(&arena,  lines[iline], ':');
-        Vec64 grabs = mutslice_by_splitter(&arena,  (mstr) game.data[1], ';');
-        b32 possible_game = True;
+        i32 game_id = iline + 1, power = 0, maxis[17] = {0};
 
-        printf(" === GAME %d ===\n", iline + 1);
+        Vec64 game = mutslice_by_splitter(&arena,lines[iline],':');
+        Vec64 grabs = mutslice_by_splitter(&arena,(mstr)game.data[1],';');
+
+        printf("     GAME %d    \n", game_id);
 
         for (int igrab = 0; igrab < grabs.len; ++igrab) {
-            Vec64 cubes = mutslice_by_splitter(&arena,  (mstr) grabs.data[igrab], ',');
+            Vec64 cubes = mutslice_by_splitter(&arena,  
+                (mstr) grabs.data[igrab], ',');
 
             for (int icube = 0; icube < cubes.len; ++ icube) {
                 mstr cube = (mstr) cubes.data[icube];
-                int n = 0;
-                char color[8] = {0};
-                b32 possible_cube = True;
+                int n = 0; char color = 'n';
+                i8 coloridx = 0;
 
-                sscanf_s(cube, " %d %s", &n, color, 8);
-                printf("|%dx%s", n, color);
+                sscanf_s(cube, " %d %c", &n, &color, 1);
+                printf("%d%c ", n, color);
 
-                switch (color[0])
-                {
-                    case 'r': possible_cube = n <= totals[RED];  break;
-                    case 'g': possible_cube = n <= totals[GREEN];  break;
-                    case 'b': possible_cube = n <= totals[BLUE];  break;
-                    default: printf("\nERROR PARSING!\n");
-                }
-
-                if (!possible_cube) {
-                    printf(" <IMPOSSIBLE> ");
-                    possible_game = False;
-                }
+                coloridx = (i8)color-'b';
+                maxis[coloridx] = n > maxis[coloridx] ? n : maxis[coloridx];
             }
-            printf("|\n");
+            printf("\n");
         }
-        
-        if (!possible_game) {
-            printf(" IMPOSSIBLE GAME! \n");
-        } else {
-            possibles += game_id;
-        }
+        powersum += power = maxis[RED]*maxis[GREEN]*maxis[BLUE];
 
-        printf(" ================= \n");
+        printf("needed: %d red %d green %d blue (%d)\n\n", maxis[RED],maxis[GREEN],maxis[BLUE], power);
     }
-
-    printf("Possibles: %d\n", possibles);
+    printf("Powersum: %lld", powersum);
 
 }
 

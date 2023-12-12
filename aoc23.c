519d5a6d7b418c01a29383c3ceb93c4537c08d24
Aleh Loup <alehloup@gmail.com>
2023-12-12 18:50:48 -0300

    prepare for AoC 10

--- aoc23.c
+++ aoc23.c
@@ -4,55 +4,20 @@
 #define print(...)  //printf(__VA_ARGS__)
 #define printn(...) //print(__VA_ARGS__); printf("\n")
 
-_fun_hot i32 turn_into_subseq(i32 sequence_len, i32 sequence[]) {
-    i32 len_minus_one = sequence_len - 1;
-
-    for (int i = 0; i < len_minus_one; ++i) {
-        sequence[i] = sequence[i+1] - sequence[i];
-    }
-    sequence[len_minus_one] = 0;
-    return sequence[0];
-}
-
 _fun i32 per_line(mstr line) {
-    static mstr numbers_str[64] = {0};
-    static i32 sequence[64] = {0}, history[64] = {0};
-    i32 numbers_str_cap = 64, sequence_len = 0, history_len = 0, seqsum = 0;
-
-    i32 numbers_str_len = split(line, ' ', numbers_str_cap, numbers_str);
-
-    for (int i = 0; i < numbers_str_len; ++i) {
-        sscanf_s(numbers_str[i], "%d ", &sequence[sequence_len++]);
-    }
-    
-    history[history_len++] = sequence[0];
-
-    while(sequence_len > 1) {
-        i32 cur = turn_into_subseq(sequence_len--, sequence);
-        history[history_len++] = cur;
-    }
-    
-    seqsum = 0;
-    for (int i = history_len - 1; i >= 0; --i) {
-        seqsum = history[i] - seqsum;
-    }
-   
-    return seqsum;
+    printf("%s \n", line);
+    return (i32) cstrlen(line);
 }
 
-//AoC 9
+//AoC 10
 _proc aoc(ci32 lines_len, mstr lines[64]) {
     i64 sum = 0;
     for (int iline = 0; iline < lines_len; ++iline) {
         mstr line = lines[iline];
-        i32 seqnum = 0;
         if (is_empty_string(line)) {
             continue;
         }
-        //printf("%s ", line);
-        seqnum = per_line(line);
-        //printf(" [%d]\n", seqnum);
-        sum += seqnum;
+        sum += per_line(line);
     }
     printf("Sum: %lld \n", sum);
 } 

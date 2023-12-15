a04c8e2074c5aff67fa2583b90f6c94ec4dd4fac
Aleh Loup <alehloup@gmail.com>
2023-12-15 20:04:33 -0300

    AoC 11

--- aoc23.c
+++ aoc23.c
@@ -5,196 +5,13 @@
 
 #define print(...)  //printf(__VA_ARGS__)
 
-#define t -77 //'^'
-#define b -77 //'v'
-#define r -16 // '>'//-81 //»
-#define l -16 //'<'//-82 //«
-
-#define I -2 // ■
-#define O -78 // ▓
-
-#define pipe -80 // ░
-
-_fun i32 count_I(len32 lines_len, mstr lines[64]) {
-    i32 count = 0;
+//AoC 11
+_proc aoc(len32 lines_len, mstr lines[64]) {
+    len32 columns_len = (len32) cstrlen(lines[0]);
+    printf("%d x %d\n", lines_len, columns_len);
     for (idx32 iline = 0; iline < lines_len; ++iline) {
-        for (idx32 icol = 0; lines[iline][icol]; ++icol) {
-            count += lines[iline][icol] == I ? 1 : 0;
-        }
-    }    
-
-    return count;
-}
-
-_proc transform_pipes(len32 lines_len, mstr lines[64]) {
-    for (idx32 iline = 0; iline < lines_len; ++iline) {
-        for (idx32 icol = 0; lines[iline][icol]; ++icol) {
-            switch(lines[iline][icol]) {
-                case '|': case 'L': case 'J': case '-': case 'F': case '7':
-                    lines[iline][icol] = pipe; break;
-                default:
-                    lines[iline][icol] = lines[iline][icol]; break;
-            }
-        }
+        printf("%s\n", lines[iline]);
     }
-}
-
-_math b32 is_path(cchar tile) {
-    return tile == t || tile == r || tile == b || tile == l;
-}
-
-_fun b32 out_of_bounds(idx32 line, idx32 col, len32 lines_len, mstr lines[64]) {
-    return (line < 0 or col < 0 or line >= lines_len or !lines[line][col]);
-}
-
-_proc fill(char char_to_fill, idx32 line, idx32 col, len32 lines_len, mstr lines[64]) {
-    if (
-        out_of_bounds(line, col, lines_len, lines)
-        || ('A' <= lines[line][col] and lines[line][col] <= 'z')
-        || is_path(lines[line][col])
-        || lines[line][col] == O
-        || lines[line][col] == I
-    ) {
-        return;
-    }
-
-    lines[line][col] = char_to_fill;
-    fill(char_to_fill, line-1, col, lines_len, lines);
-    fill(char_to_fill, line, col+1, lines_len, lines);
-    fill(char_to_fill, line+1, col, lines_len, lines);
-    fill(char_to_fill, line, col-1, lines_len, lines);
-
-    fill(char_to_fill, line-1, col-1, lines_len, lines);
-    fill(char_to_fill, line-1, col+1, lines_len, lines);
-    fill(char_to_fill, line+1, col-1, lines_len, lines);
-    fill(char_to_fill, line+1, col+1, lines_len, lines);
-}
-
-_proc fill_outs_matrix(len32 lines_len, mstr lines[64]) {
-    idx32 icol = 0;
-    for (icol = 0; lines[0][icol]; ++icol) {
-        fill(O, 0, icol, lines_len, lines);
-    }
-    for (icol = 0; lines[lines_len-1][icol]; ++icol) {
-        fill(O, lines_len-1, icol, lines_len, lines);
-    }
-    for (idx32 iline = 0; iline < lines_len; ++iline) {
-        fill(O, iline, 0, lines_len, lines);
-    }
-    for (idx32 iline = 0; iline < lines_len; ++iline) {
-        fill(O, iline, icol-1, lines_len, lines);
-    }
-}
-
-_proc fill_ins_matrix(len32 lines_len, mstr lines[64]) {
-    for (idx32 iline = 0; iline < lines_len; ++iline) {
-        for (idx32 icol = 0; lines[iline][icol]; ++icol) {
-            fill(I, iline, icol, lines_len, lines);
-        }
-    }
-}
-
-_proc print_matrix(len32 lines_len, mstr lines[64]) {
-    printf("\nMATRIX:\n");
-    for (idx32 i = 0; i < lines_len; ++i) {
-        printf("%s\n", lines[i]);
-    }
-    printf("\n");
-}
-
-//AoC 10
-_proc aoc(ci32 lines_len, mstr lines[64]) {
-    static ccstr TOPS = "|LJ";
-    static ccstr RIGHTS = "-FL";
-    static ccstr BOTTOMS = "|F7";
-    static ccstr LEFTS = "-J7";
-    
-    idx32 pos_line = 0, pos_col = 0;
-    i32 stepn = 0;
-    char last_move_dir = '.';
-
-    for (idx32 iline = 0; iline < lines_len; ++iline) {
-        ccstr line = lines[iline];
-        idx32 s_pos = -1;
-        if (is_empty_string(line)) {
-            continue;
-        }
-
-        s_pos = char_pos_in_str('S', line);
-        if (s_pos > -1) {
-            pos_line = iline; pos_col = s_pos;
-            break;
-        }        
-    }
-
-    print("Starting Position: %d %d\n", pos_line, pos_col);   
-
-    if (pos_line > 0 and char_in_(lines[pos_line-1][pos_col], BOTTOMS)) {
-        lines[pos_line][pos_col] = t;
-        --pos_line; 
-        last_move_dir = 't';
-    } else if (char_in_(lines[pos_line][pos_col+1], LEFTS)) {
-        lines[pos_line][pos_col] = r;
-        ++pos_col;
-        last_move_dir = 'r';
-    } else if (pos_line < (lines_len-1) and char_in_(lines[pos_line+1][pos_col], TOPS)) {
-        lines[pos_line][pos_col] = b;
-        ++pos_line;
-        last_move_dir = 'b';
-    } else if (pos_col > 0 and char_in_(lines[pos_line][pos_col-1], RIGHTS)) {
-        lines[pos_line][pos_col] = l;
-        --pos_col;
-        last_move_dir = 'l';
-    } else {
-        print("ERROR: could not move from start");
-    }
-
-    ++stepn;
-    print("Move: %c Step: %d Pipe: %c\n", last_move_dir, stepn, lines[pos_line][pos_col]);
-    
-    while(lines[pos_line][pos_col] != 'S') {
-        cchar cur_pipe = lines[pos_line][pos_col];
-        
-        if        (/*GO TOP*/ last_move_dir != 'b' and char_in_(cur_pipe, TOPS)) {
-            lines[pos_line][pos_col] = t;
-            --pos_line; 
-            last_move_dir = 't';
-        } else if (/*GO RIGHT*/ last_move_dir != 'l' and char_in_(cur_pipe, RIGHTS)) {
-            lines[pos_line][pos_col] = r;
-            ++pos_col;
-            last_move_dir = 'r';
-        } else if (/*GO BOTTOM*/ last_move_dir != 't' and char_in_(cur_pipe, BOTTOMS)) {
-            lines[pos_line][pos_col] = b;
-            ++pos_line;
-            last_move_dir = 'b';
-        } else if (/*GO LEFT*/ last_move_dir != 'r' and char_in_(cur_pipe, LEFTS)) {
-            lines[pos_line][pos_col] = l;
-            --pos_col;
-            last_move_dir = 'l';
-        } else {
-            print("ERROR: pipe unrecognized %c %d %d\n", cur_pipe, pos_line, pos_col);
-            print("Move: %c Step: %d Pipe: %c\n", last_move_dir, stepn, cur_pipe);
-            break;
-        }
-
-        ++stepn;
-        print("Move: %c Step: %d Pipe: %c\n", last_move_dir, stepn, lines[pos_line][pos_col]);
-    }
-
-    printf("Reached S in [%d] steps (farthest: %d) \n", (stepn - 1), stepn/2);
-
-    transform_pipes(lines_len, lines);
-
-    fill_outs_matrix(lines_len, lines);
-    fill_ins_matrix(lines_len, lines);
-
-    print_matrix(lines_len, lines);
-
-    printf("# tiles: %d\n", count_I(lines_len, lines));
-
-    // for (idx32 i = 128; i < 256; ++i) {
-    //     printf(" %c : %d (%d) \n", (char)i, i, (char)i);
-    // }    
 } 
 
 
@@ -204,7 +21,7 @@ i32 main(void) {
     i64 charbuffer_cap = 64000;
     i32 lines_cap = 256;
 
-    i32 lines_len = file_to_lines("./txts/aoc.txt", \
+    len32 lines_len = file_to_lines("./txts/aoc.txt", \
         lines_cap, lines, charbuffer_cap, charbuffer);
 
     clock_t start = clock();

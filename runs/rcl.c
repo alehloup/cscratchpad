826ad70c342cbdaccbc1c38aa6a0ee2d5a575553
Aleh Loup <alehloup@gmail.com>
2023-12-19 17:03:58 -0300

    improved runs

--- runs/rcl.c
+++ runs/rcl.c
@@ -3,7 +3,11 @@
 #include "../ale.h"
 
 i32 main(i32 argc, ccstr*argv) {
-    ccstr _1_ = argc > 1 ? argv[1] : "";
+    ccstr _1_c = argc > 1 ? argv[1] : "\0";
+    bufferchar _1_[512] = {0};
+    for (idx32 i = 0; _1_c[i] and _1_c[i] != '.'; ++i) {
+        _1_[i] = _1_c[i];
+    }
     
     shellrun(
         " msvc"
@@ -15,7 +19,7 @@ i32 main(i32 argc, ccstr*argv) {
         " && rm \"./%s.exe\""
         "\n"
         ,
-        _1_, _1_, _1_, _1_
+        _1_c, _1_, _1_, _1_
     );
 
     return 0;

3d76607e0492e096122376c0f5b9fa3cb23c108e
Aleh Loup <alehloup@gmail.com>
2023-12-20 13:54:30 -0300

    by sprintf

--- teste.c
+++ teste.c
@@ -4,7 +4,6 @@
 #define bigbuffer_cap 4096
 
 #define array_cap 16
-#define strtmp_cap 16
 
 int main(void) {
     static bufferchar bigbuffer[bigbuffer_cap] = {0};
@@ -13,13 +12,13 @@ int main(void) {
     static cstr array[array_cap] = {" "};
     static len32 array_len = 0;
 
-    static bufferchar strtmp[strtmp_cap] = {0};
-
     for (idx32 i = 0; i < 16; ++i) {
-        len32 len = sprintf_s(strtmp, 16, "123 |%d| 321", i);
-        
         array[array_len++] = &bigbuffer[bigbuffer_len];
-        bigbuffer_len += cstrcpy(&bigbuffer[bigbuffer_len], strtmp, len);
+        bigbuffer_len += sprintf_s(
+            &bigbuffer[bigbuffer_len], bigbuffer_cap, 
+            "123 |%d| 321", i
+        );
+        ++bigbuffer_len; // count the '\0'
     }
 
     for (idx32 i = 0; i < array_len; ++i) {

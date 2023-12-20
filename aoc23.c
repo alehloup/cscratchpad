b45d517ba28dd38b1d1ad256039d2440e6b35e9d
Aleh Loup <alehloup@gmail.com>
2023-12-19 23:35:52 -0300

    12.1

--- aoc23.c
+++ aoc23.c
@@ -3,16 +3,73 @@
 #include <time.h>
 #include "ale.h"
 
-_fun i32 npossibilities(len32 groups_len, i32 groups[], len32 line_len, mstr line) {
+#define set_cap 4194304
+
+_proc print_encoded(i32 x) {
+    static bufferchar str[32] = {0};
+    static len32 len = 0;
+
+    len = 0;
+
+    while (x > 1) {
+        str[len++] = x % 2 ? '#' : '.';
+        x = x >> 1;
+    }
+
+    while (len) {
+        printf("%c", str[--len]);
+    }    
+}
+
+_fun i32 encode_it(ccstr s) {
+    i32 encoded = 1;
+
+    for (idx32 i = 0; s[i]; ++i) {
+        encoded = encoded << 1;
+        encoded += s[i] == '#' ? 1 : 0;
+    }
+
+    return encoded;
+}
+
+_proc print_possibilities(char set[set_cap]) {
+    for (idx32 i = 0; i < set_cap; ++i) {
+        if (set[i]) {
+            printf("{");
+            print_encoded(i);
+            printf("}, ");
+        }            
+    }
+    printf("\n");
+}
+
+_fun i32 npossibilities(len32 groups_len, i32 groups[], len32 line_len, mstr line, i32 cur_encoded) {
+    static char set[set_cap] = {0};
     i32 npossibili = 0;
     i32 cur_group = groups_len > 0 ? groups[0] : 0;
-    
-    i32 end = line_len - cur_group;
-    
-    printf("%s|%d [%d]|%d\n", line, line_len, cur_group, groups_len);
+    i32 end = line_len - cur_group + 1;
+    b32 past_broken = False;
+    b32 print_possib = False;
 
-    if (groups_len < 1) {
-        return 1;
+    if (cur_encoded == 1) {
+        for (idx32 i = 0; i < set_cap; ++i) {
+            set[i] = 0;
+        }
+        print_possib = True;
+    }
+
+    if (groups_len == 0) {
+        for (idx32 i = 0; i < line_len; ++i) {
+            if (line[i] == '#') {
+                return 0;
+            }
+            cur_encoded = cur_encoded << 1;
+        }
+
+        if (set[cur_encoded])
+            return 0;
+
+        return set[cur_encoded] = 1;
     }
     
     if (line_len < cur_group) {
@@ -20,33 +77,64 @@ _fun i32 npossibilities(len32 groups_len, i32 groups[], len32 line_len, mstr lin
     }
 
     if (line_len == cur_group) {
-        for (idx32 i = 0; i < line_len; ++i) {
+        if (groups_len > 1) {
+            return 0;
+        }
+
+        for (idx32 i = 0; i < cur_group; ++i) {
+            cur_encoded = cur_encoded << 1;
             if (line[i] == '.') {
                 return 0;
             }
+            cur_encoded += 1;
         }
-        return 1;
+
+        if (set[cur_encoded])
+            return 0;
+        return set[cur_encoded] = 1;
     }
 
     for (idx32 i = 0; i < end; ++i) {
         b32 fit_group = True;
         i32 iplusg = i + cur_group;
-        
+
+        i32 advance_encoded = cur_encoded;
+
+        if (past_broken) {
+            break;
+        }
+
+        cur_encoded = cur_encoded << 1;
+        cur_encoded += (past_broken = line[i] == '#');
+
+
         for (idx32 j = i; j < iplusg; ++j) {
             cchar letter = line[j];
+            advance_encoded = advance_encoded << 1;
+
             if (letter == '.') {
                 fit_group = False;
-                break;
+            } else {
+                advance_encoded += 1;
             }
         }
         if (not fit_group or line[iplusg] == '#') {
             continue;
-        }
+        } else {
+            if (iplusg < line_len) {
+                advance_encoded = advance_encoded << 1;
+            }
 
-        npossibili += npossibilities(
-            groups_len - 1, &groups[1], 
-             line_len - (iplusg + 1), &line[iplusg +1 ]
-        );
+            npossibili += npossibilities(
+                groups_len - 1, &groups[1], 
+                line_len - (iplusg + 1), &line[iplusg + 1 ],
+                advance_encoded
+            );
+        }
+    }
+
+    if (print_possib) {
+        print_possibilities(set);
     }
 
     return npossibili;
@@ -59,7 +147,7 @@ _fun i32 solve_line(mstr line) {
     i32  groups[16] = {0};
     len32 groups_len = split(words[1], ',', 16, groups_str);
 
-    printf("%s ", words[0]);
+    printf("{%s} ", words[0]);
 
     for (idx32 i = 0; i < groups_len; ++i) {
         sscanf_s(groups_str[i], "%d ", &groups[i]);
@@ -69,7 +157,11 @@ _fun i32 solve_line(mstr line) {
     
     assert(words_len == 2 && "Error parsing");
     
-    return npossibilities(groups_len, groups, cstrlen32(line), line);
+    return npossibilities(
+        groups_len, groups, 
+        cstrlen32(line), line, 
+        1
+    );
 }
 
 //AoC 12
@@ -89,9 +181,9 @@ _proc aoc(len32 lines_len, mstr lines[2]) {
 
 i32 main(void) {
     static char charbuffer[Next_power2(64000)] = {0};
-    static mstr lines[256] = {0};
+    static mstr lines[1024] = {0};
     i64 charbuffer_cap = 64000;
-    i32 lines_cap = 256;
+    i32 lines_cap = 1024;
 
     len32 lines_len = file_to_lines("./txts/aoc.txt",
         lines_cap, lines, charbuffer_cap, charbuffer);

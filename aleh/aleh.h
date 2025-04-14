df54aed15f60c983996ca5e8fbe42a3fd8d6e5fe
Alessandro Stamatto <alehloup@gmail.com>
2025-04-14 03:37:18 -0300

    muittasssss funções novas, correção da arena

--- aleh/aleh.h
+++ aleh/aleh.h
@@ -1,8 +1,15 @@
-/* Heavely inspired by Aartaka Pretty C and "C Until its no Longer C"*/
-// https://aartaka.me/c-not-c
+// Heavely inspired by:
+//  Chris Nullprogram "Examples of quick hash tables and dynamic arrays in C" 
+//      https://nullprogram.com/blog/2025/01/19/
+//  Aartaka Pretty C and "C Until its no Longer C" 
+//      https://aartaka.me/c-not-c
 
 
 #pragma once
+
+// necessary to fix file and offsets to 64 
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE64_SOURCE
 
 #include <assert.h>
 #include <stdbool.h>
@@ -46,6 +53,10 @@
 #define True true
 #define False false
 
+#define KB (1024LL)
+#define MB (1024LL * 1024LL)
+#define GB (1024LL * 1024LL * 1024LL)
+
 
 extern size_t strlen(const char *);
 static inline ssize_t cstrlen(const char *s) {
@@ -71,6 +82,7 @@ static inline void * cmemset(void *s, int c, ssize_t n) {
 }
 
 
+#define macro_abs(x) ({ typeof(x) _x = (x); _x < 0 ? -_x : _x; })
 #if !defined(min) && !defined(max)
     #define min(a,b) ({ typeof(a) _a=(a); typeof(b) _b=(b); _a<_b?_a:_b; })
     #define max(a,b) ({ typeof(a) _a=(a); typeof(b) _b=(b); _a>_b?_a:_b; })
@@ -82,6 +94,8 @@ static inline void * cmemset(void *s, int c, ssize_t n) {
 #define clamp(lo, n, hi)  limit(lo, n, hi)
 
 
+#define foreach(var, array, length) \
+    for (typeof((array)[0]) *var = (array), *var##_end_ = (array) + (length); var < var##_end_; ++var)
 #define fortimes(var, times) \
     for (typeof(times) var##_max_ = (times), var = 0; var < var##_max_; ++var)
 #define forasc(var, from, to) \
@@ -92,8 +106,6 @@ static inline void * cmemset(void *s, int c, ssize_t n) {
     for (typeof(to) var = (from), var##_to_ = (to); var < var##_to_; var += (by))
 #define fordescby(var, from, to, by) \
     for (typeof(from) var = (from), var##_to_ = (to); var > var##_to_; var -= (by))
-#define foreach(var, array, length) \
-    for (typeof((array)[0]) *var = (array), *var##_end_ = (array) + (length); var < var##_end_; ++var)
 #define for_i(times) forasc(i, 0, times)
 #define for_j(times) forasc(j, 0, times)
 #define for_k(times) forasc(k, 0, times)
@@ -112,25 +124,224 @@ static inline void* alloc(arena *a, ptrdiff_t count, ptrdiff_t size, ptrdiff_t a
     ptrdiff_t pad = (ptrdiff_t)( -(uintptr_t)a->beg & (uintptr_t)(align-1) );
     
     assert( count < (a->end - a->beg - pad)/size and "arena space left is not enough" );
+    ssize_t total_size = count*size;
     
-    void *r = a->beg + count*size;
-    a->beg += pad + count*size;
-    
-    return cmemset(r, 0, count*size);
+    void *r = a->beg + pad;
+    a->beg += pad + total_size;
+
+    // zero at-most 64KB to avoid mega memsets...    
+    return cmemset(r, 0, min(total_size, 64*KB));
 }
 #define new(a, n, t) ((t *)(alloc(a, n, (ptrdiff_t)sizeof(t), alignof(t))))
+#define arenaarr(arr) ({ (arena){arr, arr + countof(arr)}; })
 
 
 typedef struct str { char *data; ptrdiff_t len; } str;
 #define S(s) ({(str){(char *)s, countof(s)-1};})
 
-static inline int str_equal(str a, str b) {
+static inline int sequal(str a, str b) {
     return (a.len != b.len) ? 0 : !cmemcmp(a.data, b.data, a.len);
 }
 
-static inline str str_copy(arena *a, str original) {
+static inline str copy(arena *a, str original) {
     str copied = original;
     copied.data = new(a, copied.len, char);
     cmemcpy(copied.data, original.data, copied.len);
     return copied;
+}
+
+static inline str cat(arena *a, str head, str tail) {
+    // use copy as realoc if head is not at the top of arena
+    if (!head.data or (head.data + head.len) != a->beg) {
+        head = copy(a, head);
+    }
+    
+    // since head is always at the top of arena, copy extends it
+    head.len += copy(a, tail).len;
+    
+    return head;
+}
+
+static inline str span(char *beg, char *end) {
+    return (str){beg, beg ? end - beg : 0};
+}
+
+typedef struct {str head; str tail; int ok; int padding;} head_tail_ok;
+// cut s at c, returns {str head; str tail; int ok;}
+static inline head_tail_ok cut(str s, char c) {
+    head_tail_ok r = {0};
+    if (!s.len) return r;
+
+    char *it = s.data;
+    char *end = s.data + s.len;
+    for (; it < end and *it != c; ++it) {}
+
+    r.ok = it < end;
+    r.head = span(s.data, it);
+    r.tail = span(it + r.ok, end);
+    return r;
+}
+
+static inline str trimleft(str s) {
+    for (; s.len and (unsigned char)*s.data <= ' '; ++s.data, --s.len);
+    return s;
+}
+static inline str trimright(str s) {
+    for (; s.len and (unsigned char)s.data[s.len-1] <= ' '; --s.len);
+    return s;
+}
+static inline str trim(str s) {
+    return trimleft(trimright(s));
+}
+
+static inline str substring(str s, ssize_t i) {
+    if (i > 0) {
+        s.data += i;
+        s.len -= i;
+    }
+    return s;
+}
+
+static inline int starts(str s, str prefix) {
+    return (s.len >= prefix.len) \
+        and sequal(span(s.data, s.data + prefix.len), prefix);
+}
+static inline int ends(str s, str suffix) {
+    return (s.len >= suffix.len) \
+        and sequal(substring(s, s.len - suffix.len), suffix);
+}
+
+static inline str scanword(arena *a) {
+    static char buffer[256];
+    (void)scanf(" %255s", buffer);
+
+    ssize_t len = cstrlen(buffer);
+    char *data = new(a, len + 1, char);
+    cmemcpy(data, buffer, len);
+    data[len] = 0;
+
+    return (str){ data, len };
+}
+static inline str scanline(arena *a) {
+    static char buffer[1024];
+    (void)scanf(" %1023[^\n]", buffer);
+
+    ssize_t len = cstrlen(buffer);
+    char *data = new(a, len + 1, char);
+    cmemcpy(data, buffer, len);
+    data[len] = 0;
+
+    return (str){ data, len };
+}
+
+
+static inline int char_equal(char a, char b) { return a == b; }
+static inline int int_equal(int a, int b) { return a == b; }
+static inline int ssize_equal(ssize_t a, ssize_t b) { return a == b; }
+static inline int size_equal(size_t a, size_t b) { return a == b; }
+static inline int float_equal(float a, float b)
+{
+	float abs_a = macro_abs(a);
+	float abs_b = macro_abs(b);
+	float diff  = macro_abs(a - b);
+
+	static const float epsilon = 1e-5f;
+	static const float min_abs = 1e-8f;
+	if (abs_a < min_abs && abs_b < min_abs) {
+		// Both numbers are extremely close to 0
+		return diff < min_abs;
+	}
+
+	// Use relative comparison
+	return diff <= epsilon * max(abs_a, abs_b);
+}
+static inline int double_equal(double a, double b)
+{
+	double abs_a = macro_abs(a);
+    double abs_b = macro_abs(b);
+	double diff  = macro_abs(a - b);
+
+	static const double epsilon = 1e-9;
+	static const double min_abs = 1e-12;
+
+	if (abs_a < min_abs && abs_b < min_abs) {
+		// Both numbers are extremely close to 0
+		return diff < min_abs;
+	}
+
+	// Use relative comparison
+	return diff <= epsilon * max(abs_a, abs_b);
+}
+
+// don't use char* as strings, so no equal for it!
+#define equal(a, b) \
+    _Generic((a), \
+        char: char_equal, int: int_equal, \
+        ssize_t: ssize_equal, size_t: size_equal, \
+        float: float_equal, double: double_equal, \
+        str: sequal \
+    )(a, b)
+
+static inline void print_char(char c) { printf("%c", c); }
+static inline void print_int(int i) { printf("%d", i); }
+static inline void print_ssize(ssize_t s) { printf("%zd", s); }
+static inline void print_size(size_t z) { printf("%zu", z); }
+static inline void print_float(float f) { printf("%.5f", (double)f); }
+static inline void print_double(double d) { printf("%.9f", d); }
+static inline void print_str(str s) { printf("%.*s", (int)s.len, s.data); }
+
+#define print(a) \
+    _Generic((a), \
+        char: print_char, int: print_int, \
+        ssize_t: print_ssize, size_t: print_size, \
+        float: print_float, double: print_double, \
+        str: print_str \
+    )(a)
+#define println(a) print(a); printf("\n")
+
+static inline void scan_char(char *c) { (void)scanf(" %c", c); }
+static inline void scan_int(int *i) { (void)scanf(" %d", i); }
+static inline void scan_ssize(ssize_t *s) { (void)scanf(" %zd", s); }
+static inline void scan_size(size_t *z) { (void)scanf(" %zu", z); }
+static inline void scan_float(float *f) { (void)scanf(" %f", f); }
+static inline void scan_double(double *d) { (void)scanf(" %lf", d); }
+
+#define scan(a) \
+    _Generic((a), \
+        char: scan_char, int: scan_int, \
+        ssize_t: scan_ssize, size_t: scan_size, \
+        float: scan_float, double: scan_double \
+    )(&a)
+
+static inline size_t hash_str(str s, size_t seed) {
+    size_t h = seed;
+    for_i(s.len) {
+        h ^= s.data[i] & 255;
+        h *= 11111111111;
+    }
+    return h;
+}
+
+static inline str filestr(arena * a, str filename) {
+    FILE* file = fopen(filename.data, "rb");
+    if (!file) return (str){0};
+
+        fseek(file, 0, SEEK_END);
+        ssize_t size = (ssize_t)ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        if (size <= 0) {
+            printf("Could not get file size: %zd\n", size);
+            fclose(file);
+            return (str){0};
+        }
+
+        char *data = new(a, size + 1, char);
+
+        ssize_t read_len = (ssize_t)fread(data, 1, (size_t)size, file);
+        data[read_len] = 0;
+
+    fclose(file);
+
+    return (str){data, read_len};
 }
--- teste.c
+++ teste.c
@@ -3,28 +3,60 @@
 #include <string.h>
 #include <stdlib.h>
 
-static void print_str(arena a) {
+static void test_strings(arena a) {
+    str string = S("Alessandro Stamatto");
+    printf("%s %zd \n", string.data, string.len);
+    printf("%d \n", equal(S("Alessandro Stamatt"), string));
+
     str *pstring = new(&a, 1, str);
 
     pstring->data = (char *) "Alessandro";
     pstring->len = cstrlen(pstring->data);
 
     printf("%s %zd\n", pstring->data, pstring->len);
+
+    str string2 = copy(&a, string);
+    printf("%d %s %s \n", equal(string, string2), string.data, string2.data);
+    string2.data[3] = 'h';
+    printf("%d %s %s \n", equal(string, string2), string.data, string2.data);
 }
 
+#define print_equals(format, a, b) \
+    printf(format " == " format " (%d)\n", a, b, equal(a, b)); \
+    printf(format " == " format " (%d)\n", a, b+1, equal(a, b+1))
+
+static void test_equals() {
+    char ca = 'a', cb = 'a';
+    int ia = 5, ib = 5;
+    ssize_t sa = -52, sb = -52;
+    size_t ua = 52, ub = 52;
+    float fa = 5.2f, fb = 5.2f;
+    double da = 52.52, db = 52.52;
+
+    print_equals("%c", ca, cb);
+    print_equals("%d", ia, ib);
+    print_equals("%zd", sa, sb);
+    print_equals("%zu", ua, ub);
+    print_equals("%f", (double) fa, (double) fb);
+    print_equals("%f", da, db);
+}
+
+#define ROOTWIN "D:/"
+#define ROOTLIN "/mnt/d/"
+
 int main(void) {
-    str string = S("Alessandro Stamatto");
-    printf("%s %zd \n", string.data, string.len);
-    printf("%d \n", str_equal(S("Alessandro Stamatt"), string));
+    char *data = malloc(16*GB);
+    arena a = {data, data + 16*GB};
+    
+    test_strings(a);
+    test_equals();
+    printf("\n");
 
-    char bytes[52] = {0};
-    arena a = {bytes, bytes + 52};
-    print_str(a);
+    str contents = filestr(&a, S(ROOTWIN "1brc_java/measurements10k.txt"));
+    println(contents.len);
 
-    str string2 = str_copy(&a, string);
-    printf("%d %s %s \n", str_equal(string, string2), string.data, string2.data);
-    string2.data[3] = 'h';
-    printf("%d %s %s \n", str_equal(string, string2), string.data, string2.data);
+    contents = filestr(&a, S(ROOTLIN "1brc_java/measurements10k.txt"));
+    println(contents.len);
 
     return 0;
 }
